bd37dd0572924557765d542be8f285b6b172347a
Jay Chu <tothesong@gmail.com>
2019-08-17 14:42:22 +0800

    1.3

---
+++ 1.5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Enter two numbers:";
+  std::cout << std::endl;
+  int v1 = 0, v2 = 0;
+  std::cin >> v1;
+  std::cin >> v2;
+  std::cout << "The first is:";
+  std::cout << v1;
+  std::cout << std::endl;
+  std::cout << "The second is:";
+  std::cout << v2;
+  std::cout << std::endl;
+  return 0;
+}
