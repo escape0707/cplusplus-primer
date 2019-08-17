75d609c982170002c36a7b0fc09e73baf72f5f6f
Jay Chu <tothesong@gmail.com>
2019-08-17 21:27:33 +0800

    Change tabSize to 2

--- 1.11.cpp
+++ 1.11.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
 int main() {
-    int v1 = 0, v2 = 0;
-    std::cout << "Enter two numbers:" << std::endl;
-    std::cin >> v1 >> v2;
-    int step = v1 < v2 ? 1 : -1;
-    while (v1 != v2) {
-        std::cout << v1 << ' ';
-        v1 += step;
-    }
-    std::cout << v2 << std::endl;
-    return 0;
+  int v1 = 0, v2 = 0;
+  std::cout << "Enter two numbers:" << std::endl;
+  std::cin >> v1 >> v2;
+  int step = v1 < v2 ? 1 : -1;
+  while (v1 != v2) {
+    std::cout << v1 << ' ';
+    v1 += step;
+  }
+  std::cout << v2 << std::endl;
+  return 0;
 }
