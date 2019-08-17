89f5507f800ceea5e2de352e844faff30d096149
Jay Chu <tothesong@gmail.com>
2019-08-17 22:13:23 +0800

    1.4.2

---
+++ 1.13.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+  int sum = 0;
+  // keep executing the for as long as val is less than or equal to 10
+  for (int val = 1; val <= 10; ++val) {
+    sum += val;
+  }
+  std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
+  return 0;
+}
