eb5654f45b38295923dc4c420cef3f06d77a1473
Jay Chu <tothesong@gmail.com>
2019-08-18 15:19:43 +0800

    1.5.1

---
+++ 1.22.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main() {
+  Sales_item book;
+  if (std::cin >> book) {
+    Sales_item currTrans;
+    while (std::cin >> currTrans) {
+      book += currTrans;
+    }
+    std::cout << book << std::endl;
+  }
+  return 0;
+}
