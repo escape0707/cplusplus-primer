e5d5fab9bf8ed1fe739191c574cf5ed4e4d16e0a
Jay Chu <tothesong@gmail.com>
2019-08-22 21:33:39 +0800

    2.5.2

---
+++ 2.33.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int i = 0, &r = i;
+auto a = r;  // a is an int (r is an alias for i, whichh has type int)
+
+const int ci = i, &cr = ci;
+auto b = ci;   // b is an int (top-level const in ci is dropped)
+auto c = cr;   // c is an int (cr is an alias for ci whose const is top-level)
+auto d = &i;   // d is an int* (& of an int object is int*)
+auto e = &ci;  // e is const int* (& of a const object is low-level const)
+
+const auto f = ci;  // deduced type of ci is int; f has type const int
+
+auto &g = ci;  // g is a const int& that is bound to ci
+
+void print() {
+  std::cout << a << ' ' << b << ' ' << c << ' ' << d << ' ' << e << ' ' << g
+            << std::endl;
+}
+
+int main() {
+  print();
+  a = 42;
+  print();
+  b = 42;
+  print();
+  c = 42;
+  print();
+}
---
+++ 2.35.cpp
@@ -0,0 +1,7 @@
+int main() {
+  const int i = 42;
+  auto j = i;
+  const auto &k = i;
+  auto *p = &i;
+  const auto j2 = i, &k2 = i;
+}
