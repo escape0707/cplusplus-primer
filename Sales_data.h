117ea42568eb206d589abd48f741e62c29c06943
Jay Chu <tothesong@gmail.com>
2019-08-23 21:20:08 +0800

    2.6.3

--- Sales_data.h
+++ Sales_data.h
@@ -1,3 +1,6 @@
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+
 #include <string>
 
 struct Sales_data {
@@ -5,3 +8,4 @@ struct Sales_data {
   unsigned units_sold = 0;
   double revenue = 0.0;
 };
+#endif
