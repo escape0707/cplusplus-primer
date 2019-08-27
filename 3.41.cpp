9b9896d91263c078faed14daadd405bd2d930db5
Jay Chu <tothesong@gmail.com>
2019-08-27 16:38:21 +0800

    3.5.5

---
+++ 3.41.cpp
@@ -0,0 +1,21 @@
+#include <cstddef>
+#include <cstring>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+using std::begin, std::end, std::cbegin, std::cend;
+using std::cin, std::cout, std::endl;
+using std::size_t, std::ptrdiff_t;
+using std::string;
+using std::strlen, std::strcmp, std::strcat, std::strcpy;
+using std::vector;
+
+int main() {
+  int int_arr[] = {0, 1, 2, 3, 4, 5};
+  // ivec has six elements; each is a copy of the corresponding element in int_arr
+  vector<int> ivec(begin(int_arr), end(int_arr));
+
+  return 0;
+}
---
+++ 3.42.cpp
@@ -0,0 +1,29 @@
+#include <cstddef>
+#include <cstring>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+using std::begin, std::end, std::cbegin, std::cend;
+using std::cin, std::cout, std::endl;
+using std::size_t, std::ptrdiff_t;
+using std::string;
+using std::strlen, std::strcmp, std::strcat, std::strcpy;
+using std::vector;
+
+int main() {
+  vector<int> ivec = {0, 1, 2, 3, 4, 5};
+  int arr[ivec.size()];
+  int *p = arr;
+  for (const auto &i : ivec) {
+    *p++ = i;
+  }
+
+  for (const auto &i : arr) {
+    cout << i << ' ';
+  }
+  cout << endl;
+
+  return 0;
+}
