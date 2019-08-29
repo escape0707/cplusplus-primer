23af7ef8cabc4e05befc3837bf1127d4fc23d3da
Jay Chu <tothesong@gmail.com>
2019-08-29 20:23:26 +0800

    4.1.2

---
+++ 4.1.cpp
@@ -0,0 +1,19 @@
+#include <cstddef>
+#include <cstring>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+using std::begin, std::end, std::cbegin, std::cend;
+using std::cin, std::cout, std::endl;
+using std::size_t, std::ptrdiff_t;
+using std::string;
+using std::strlen, std::strcmp, std::strcat, std::strcpy;
+using std::vector;
+
+int main() {
+  cout << 5 + 10 * 20 / 2 << endl; // 105
+
+  return 0;
+}
