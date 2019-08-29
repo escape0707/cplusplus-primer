8d8a25c5f02e1cfad2aa9a0b3355b666ea4c93ff
Jay Chu <tothesong@gmail.com>
2019-08-29 20:54:37 +0800

    Update 4.1.2

--- 4.2.cpp
+++ 4.2.cpp
@@ -1,2 +1,11 @@
-// *((vec.begin)())
-// (*((vec.begin)())) + 1
+#include <iostream>
+#include <vector>
+
+using std::cin, std::cout, std::endl;
+using std::vector;
+
+int main() {
+  vector<int> vec = {0, 1, 2};
+  cout << *((vec.begin)()) << ' ' << *vec.begin() << endl;
+  cout << (*((vec.begin)())) + 1 << ' ' << *vec.begin() + 1 << endl;
+}
