616796538fef0c109b3df1abba0a5802a6c32618
Jay Chu <tothesong@gmail.com>
2019-09-02 23:01:00 +0800

    5.2

---
+++ 5.4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin, std::cout, std::endl;
+using std::string;
+using std::vector;
+
+int main() {
+	string s;
+	while (string::iterator iter != s.end()) { /* ... */
+	}                                          // no initialization for iter
+	while (bool status = find(word)) {         /* ... */
+	}
+	if (!status) { /* ... */
+	}              // out of scope
+
+	return 0;
+}
+
