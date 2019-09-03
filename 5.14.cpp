3c53e565fa3d458b55fee98719662bbec38514b5
Jay Chu <tothesong@gmail.com>
2019-09-03 20:45:27 +0800

    5.4.1

---
+++ 5.14.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin, std::cout, std::endl;
+using std::string;
+using std::vector;
+
+int main() {
+  string freq;
+  int freq_cnt = 0;
+
+  string prev;
+  if (cin >> prev) {
+    int cnt = 1;
+    string curr;
+    while (cin >> curr) {
+      if (curr == prev) {
+        ++cnt;
+      } else {  // curr != prev
+        if (cnt > freq_cnt) {
+          freq = prev;
+          freq_cnt = cnt;
+        }
+        // reset prev & cnt
+        prev = curr;
+        cnt = 1;
+      }
+    }
+    // process last word-count result
+    if (cnt > freq_cnt) {
+      // update freq & freq_cnt
+      freq = prev;
+      freq_cnt = cnt;
+    }
+    cout << freq << ": " << freq_cnt << endl;
+  } else {  // no input!
+    cout << "No input." << endl;
+  }
+
+  return 0;
+}
