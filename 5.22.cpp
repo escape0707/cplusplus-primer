853f6d47175096dda6113413232778441748f199
Jay Chu <tothesong@gmail.com>
2019-09-04 11:49:41 +0800

    5.5.3

---
+++ 5.22.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin, std::cout, std::endl;
+using std::string;
+using std::vector;
+
+int get_size() {
+  return 42;
+}
+
+int main() {
+  for (int sz = get_size(); sz <= 0;) {
+  }
+
+  return 0;
+}
