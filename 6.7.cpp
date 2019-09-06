3cb8bdc62609b4789a48c4ef66e7eb4854f66bf3
Jay Chu <tothesong@gmail.com>
2019-09-06 14:22:22 +0800

    6.1.1

---
+++ 6.7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin, std::cout, std::endl;
+using std::string;
+using std::vector;
+
+size_t count_calls() {
+	static size_t ctr = 0; // value will persist across calls
+	return ctr++;
+}
+
+int main() {
+	for (size_t i = 0; i != 10; ++i) {
+		cout << count_calls() << endl;
+	}
+
+	return 0;
+}
