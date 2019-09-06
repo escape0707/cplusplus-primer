e195c8a57cd2aa751a3256d93e8d317e2551a860
Jay Chu <tothesong@gmail.com>
2019-09-06 14:10:35 +0800

    6.1

---
+++ 6.2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin, std::cout, std::endl;
+using std::string;
+using std::vector;
+
+int f() {
+  string s;
+  // ...
+  return s;
+}
+
+f2(int i) { /* ... */ }
+
+int calc(int v1, int v1) { /* ... */ }
+
+double square(double x) return x * x;
+
+int main() {
+
+  return 0;
+}
---
+++ 6.5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin, std::cout, std::endl;
+using std::string;
+using std::vector;
+
+int abs(int val) {
+	return (val < 0) ? -val : val;
+}
+
+int main() {
+	int val;
+	while (cin >> val) {
+		cout << abs(val) << endl;
+	}
+
+	return 0;
+}
+
