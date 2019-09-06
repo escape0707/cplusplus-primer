0a1ff49aa8f994ed3618dc709a1c84cc9aeb624a
Jay Chu <tothesong@gmail.com>
2019-09-06 18:48:02 +0800

    6.2.1

---
+++ 6.10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin, std::cout, std::endl;
+using std::string;
+using std::vector;
+
+void swap(int *a, int *b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+int main() {
+  int v1 = 0, v2 = 1;
+  swap(&v1, &v2);
+  cout << v1 << ' ' << v2 << endl;
+
+  swap(&v1, &v2);
+  cout << v1 << ' ' << v2 << endl;
+
+  return 0;
+}
