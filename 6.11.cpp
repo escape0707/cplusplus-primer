0cfa2a9feeca13df0ad8125bd3ddccc4f0026a55
Jay Chu <tothesong@gmail.com>
2019-09-07 18:20:14 +0800

    6.2.2

---
+++ 6.11.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin, std::cout, std::endl;
+using std::string;
+using std::vector;
+
+void reset(int &i) {
+  i = 0;
+}
+
+int main() {
+  int val = 1;
+  cout << val << endl;
+  reset(val);
+  cout << val << endl;
+
+  return 0;
+}
