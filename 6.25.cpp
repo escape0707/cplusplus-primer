b8c0711d96fd6eed7a080849b854932ba8df8f4d
Jay Chu <tothesong@gmail.com>
2019-09-07 22:33:50 +0800

    6.2.5

---
+++ 6.25.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+using std::cbegin, std::cend;
+using std::cin, std::cout, std::endl;
+using std::string;
+using std::vector;
+
+int main(int argc, char **argv) {
+  string result;
+  for (char **sp = argv + 1; *sp; ++sp) {
+    result += *sp;
+  }
+  cout << result << endl;
+
+  return 0;
+}
