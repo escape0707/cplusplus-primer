59150f09d578d77ca69f208450ee612a8e4b01b2
Jay Chu <tothesong@gmail.com>
2019-09-12 17:12:54 +0800

    6.7

---
+++ 6.54.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin, std::cout, std::endl;
+using std::string;
+using std::vector;
+
+int func(int, int);
+
+vector<int (*)(int, int)> fpvec;
+
+int main() {
+}
---
+++ 6.55.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin, std::cout, std::endl;
+using std::string;
+using std::vector;
+
+int func(int, int) {
+  return 42;
+}
+
+int add(int a, int b) {
+  return a + b;
+}
+int subtract(int a, int b) {
+  return a - b;
+}
+int multiply(int a, int b) {
+  return a * b;
+}
+int divide(int a, int b) {
+  return a / b;
+}
+
+vector<int (*)(int, int)> fpvec = {add, subtract, multiply, divide};
+
+int main() {
+  // for (const auto &elem : fpvec) {
+  for (int (*const &elem)(int, int) : fpvec) {
+    cout << elem(42, 10) << ' ';
+  }
+  cout << endl;
+}
