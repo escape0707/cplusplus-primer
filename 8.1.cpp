203d86f51bc8f3bd35bacca1a41f85c0f21646f3
Jay Chu <tothesong@gmail.com>
2019-09-25 20:37:24 +0800

    Update 8.1.cpp

--- 8.1.cpp
+++ 8.1.cpp
@@ -8,11 +8,22 @@ using std::string;
 using std::vector;
 
 istream &process_input(istream &is) {
-  string buf;
-  while (is >> buf) {
-    cout << buf;
+  cout << is.rdbuf();  // write everything in is buffer into cout
+
+  // ignore everything still in is's buffer, then reset is's rdstate().
+  // note that the ignore() below won't set failbit even if EOF is reached.
+  // after all, it succeeded to "ignore until and including EOF".
+  // and istream's bool operator (invoked when use istream object as bool)
+  // returns !fail(), which is true when rdstate() equals goodbit or eofbit
+  is.ignore(std::numeric_limits<std::streamsize>::max());
+  if (is) {
+    is.clear();
   }
-  return is;
+
+  // if the correct rdstate is needed, maybe it's ok and convenient to invoke
+  // .peek() immediately after .ignore()
+
+  return is;  // return is stream
 }
 
 int main() {
