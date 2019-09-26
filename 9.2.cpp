ab859d011aec632909ba409c4f274bfc2c631234
Jay Chu <tothesong@gmail.com>
2019-09-27 00:25:51 +0800

    9.2

---
+++ 9.2.cpp
@@ -0,0 +1,7 @@
+#include <deque>
+#include <list>
+
+using std::deque;
+using std::list;
+
+list<deque<int>> li;
--- template.cpp
+++ template.cpp
@@ -1,13 +1,8 @@
-#include <fstream>
 #include <iostream>
-#include <sstream>
 #include <string>
 #include <vector>
 
 using std::cin, std::cout, std::endl;
-using std::getline;
-using std::ifstream, std::ofstream;
-using std::istringstream, std::ostringstream;
 using std::string;
 using std::vector;
 
