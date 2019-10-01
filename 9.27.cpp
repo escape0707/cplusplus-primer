8cbe19cd09032a650a57232e7386b570e0363af1
Jay Chu <tothesong@gmail.com>
2019-10-01 18:16:12 +0800

    9.3.4

---
+++ 9.27.cpp
@@ -0,0 +1,23 @@
+#include <forward_list>
+#include <iostream>
+
+using std::cout, std::endl;
+using std::forward_list;
+
+int main() {
+  forward_list<int> iflist{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  for (auto prev = iflist.cbefore_begin(), curr = iflist.cbegin();
+       curr != iflist.cend();) {
+    if (*curr & 1) {
+      curr = iflist.erase_after(prev);
+    } else {
+      prev = curr++;
+    }
+  }
+
+  for (const auto &elem : iflist) {
+    cout << elem << ' ';
+  }
+  cout << endl;
+}
---
+++ 9.28.cpp
@@ -0,0 +1,51 @@
+#include <forward_list>
+#include <iostream>
+#include <string>
+
+using std::cout, std::endl, std::cerr;
+using std::forward_list;
+using std::ostream;
+using std::string;
+
+forward_list<string>::const_iterator find(const forward_list<string> &sflist,
+                                          const string &str) {
+  auto prev = sflist.cbefore_begin();
+  for (auto curr = sflist.cbegin(); curr != sflist.cend(); prev = curr++) {
+    if (*curr == str) {
+      return curr;
+    }
+  }
+  return prev;  // which is sflist.cend()-1;
+}
+
+void insert_after(forward_list<string> &sflist,
+                  const string &dest,
+                  string &&src) {
+  sflist.insert_after(find(sflist, dest), std::move(src));
+  cerr << "move" << endl;
+}
+
+void insert_after(forward_list<string> &sflist,
+                  const string &dest,
+                  const string &src) {
+  sflist.emplace_after(find(sflist, dest), src);
+  cerr << "construct" << endl;
+}
+
+ostream &operator<<(ostream &os, const forward_list<string> &text) {
+  for (const auto &elem : text) {
+    cout << elem << ' ';
+  }
+  return os;
+}
+
+int main() {
+  forward_list<string> text1{"Hello", "world!"};
+
+  insert_after(text1, "Hello", "to the");
+  cout << text1 << endl;
+
+  string s("Bob!");
+  insert_after(text1, "Hi!", s);
+  cout << text1 << endl;
+}
