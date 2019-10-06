b1befbd847ea728d13f72ada61762883a714bbcd
Jay Chu <tothesong@gmail.com>
2019-10-06 17:50:46 +0800

    9.6

---
+++ 9.52.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <stack>
+#include <string>
+
+using std::copy;
+using std::cout, std::endl;
+using std::ostream_iterator;
+using std::stack;
+using std::string;
+
+int main() {
+  // Parenthesized expression.
+  const string s("This is (Some (awesome)((((wooooooooo))))) and (ocxs) over");
+  stack<char> stk;     // Stack to hold process result.
+  unsigned count = 0;  // Counter for '('.
+
+  for (const char &c : s) {
+    if (c == ')' && count) {
+      --count;
+      while (stk.top() != '(') {
+        stk.pop();
+      }
+      // change the '(' to '#' to indicate that the expression was replaced
+      stk.top() = '#';
+    } else {
+      if (c == '(') {
+        ++count;
+      }
+      stk.push(c);
+    }
+  }
+
+
+  string rev;  // Cached content of stack in reverse order
+  rev.reserve(stk.size());
+  // cache content of stack in reverse order
+  while (!stk.empty()) {
+    rev.push_back(stk.top());
+    stk.pop();
+  }
+
+  // print result in original order
+  copy(rev.crbegin(), rev.crend(), ostream_iterator<char>(cout));
+  cout << endl;
+}
