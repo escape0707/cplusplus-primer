3965e727fb5c1feda9accc476ca129a52da11e5b
Jay Chu <tothesong@gmail.com>
2019-10-10 22:37:04 +0800

    11.2.1

---
+++ 11.7.cpp
@@ -0,0 +1,27 @@
+#include <map>
+#include <utility>
+#include <string>
+#include <iostream>
+#include <vector>
+
+using std::cout, std::endl;
+using std::map;
+using std::pair;
+using std::string;
+using std::vector;
+
+int main() {
+  map<string, vector<string>> families;
+
+  families["Simpson"] = {"Homer", "Marge"};
+  families["Simpson"].push_back("Bart");
+  families["Simpson"].insert(families["Simpson"].end(), {"Lisa", "Maggie"});
+
+  for (const pair<string, vector<string>> &family : families) {
+    cout << "The " << family.first << "s: ";
+    for (const string &name : family.second) {
+      cout << name << ' ';
+    }
+    cout << endl;
+  }
+}
---
+++ 11.8.cpp
@@ -0,0 +1,35 @@
+// set find element faster
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+using std::cin, std::cout, std::endl;
+using std::find;
+using std::istream_iterator, std::ostream_iterator;
+using std::map;
+using std::pair;
+using std::string;
+using std::vector;
+
+int main() {
+  map<string, size_t> word_count;
+  vector<string> exclude = {"The", "But", "And", "Or", "An", "A",
+                            "the", "but", "and", "or", "an", "a"};
+
+  for (istream_iterator<string> it(cin), eof; it != eof; ++it) {
+    if (find(exclude.cbegin(), exclude.cend(), *it) == exclude.cend()) {
+      // if (exclude.find(*it) == exclude.cend()) {
+      ++word_count[*it];
+    }
+  }
+
+  for (const pair<string, size_t> &p : word_count) {
+    cout << p.first << " occurs " << p.second
+         << (p.second > 1 ? " times" : " time") << endl;
+  };
+}
