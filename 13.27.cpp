cf78817d555145913f2d7de453e7046f104be7e2
escape0707 <tothesong@gmail.com>
2019-11-08 17:37:27 +0800

    13.2.2

---
+++ 13.27.cpp
@@ -0,0 +1,1 @@
+// see HasPtr.h
--- HasPtr.h
+++ HasPtr.h
@@ -1,23 +1,38 @@
 #ifndef HASPTR_H
 #define HASPTR_H
 
+#include <cstddef>
 #include <string>
 
 class HasPtr {
  public:
   HasPtr() = default;
   HasPtr(const std::string &s) : ps(new std::string(s)) {}
-  HasPtr(const HasPtr &other) : ps(new std::string(*other.ps)), i(other.i) {}
+  HasPtr(const HasPtr &other) : ps(other.ps), i(other.i), use(other.use) {
+    ++*use;
+  }
   HasPtr &operator=(const HasPtr &rhs) {
-    *ps = *rhs.ps;
+    ++*rhs.use;
+    if (--*use == 0) {
+      delete use;
+      delete ps;
+    }
+    ps = rhs.ps;
     i = rhs.i;
+    use = rhs.use;
     return *this;
   }
-  ~HasPtr() { delete ps; }
+  ~HasPtr() {
+    if (--*use == 0) {
+      delete use;
+      delete ps;
+    }
+  }
 
  private:
   std::string *ps = new std::string();
   int i = 0;
+  std::size_t *use = new std::size_t(1);
 };
 
 #endif
