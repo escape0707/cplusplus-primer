ec5e1277b919fd0b8af1bab06dea0552eef409c6
escape0707 <tothesong@gmail.com>
2019-11-10 17:05:26 +0800

    13.5 (1): 13.39 base

---
+++ StrVec.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <memory>
+#include <utility>
+
+#include "StrVec.h"
+
+using std::destroy;
+using std::pair;
+using std::uninitialized_copy, std::uninitialized_move;
+
+using size_type = StrVec::size_type;
+using iterator = StrVec::iterator;
+
+StrVec::StrVec() = default;
+
+StrVec::StrVec(const StrVec &other) {
+  pair<iterator, iterator> newdata = alloc_n_copy(other.begin(), other.end());
+  elements = newdata.first;
+  first_free = cap = newdata.second;
+}
+
+StrVec &StrVec::operator=(const StrVec &rhs) {
+  pair<iterator, iterator> newdata = alloc_n_copy(rhs.begin(), rhs.end());
+  free();
+  elements = newdata.first;
+  first_free = cap = newdata.second;
+  return *this;
+}
+
+StrVec::~StrVec() {
+  free();
+}
+
+void StrVec::push_back(const_reference value) {
+  chk_n_alloc();
+  Alloc_traits::construct(alloc, first_free++, value);
+}
+
+size_type StrVec::size() const {
+  return first_free - elements;
+}
+
+size_type StrVec::capacity() const {
+  return cap - elements;
+}
+
+iterator StrVec::begin() const {
+  return elements;
+}
+
+iterator StrVec::end() const {
+  return first_free;
+}
+
+pair<iterator, iterator> StrVec::alloc_n_copy(const_iterator beg,
+                                                 const_iterator end) {
+  iterator newbeg = Alloc_traits::allocate(alloc, end - beg);
+  return {newbeg, uninitialized_copy(beg, end, newbeg)};
+}
+
+pair<iterator, iterator> StrVec::alloc_n_move(iterator beg, iterator end) {
+  iterator newbeg = Alloc_traits::allocate(alloc, end - beg);
+  return {newbeg, uninitialized_move(beg, end, newbeg)};
+}
+
+void StrVec::chk_n_alloc() {
+  if (first_free == cap) {
+    reallocate();
+  }
+}
+
+void StrVec::free() {
+  if (elements) {
+    destroy(elements, first_free);
+    Alloc_traits::deallocate(alloc, elements, capacity());
+  }
+}
+
+void StrVec::reallocate() {
+  size_type newcapacity = size() ? 2 * size() : 1;  // todo
+  iterator newelements = Alloc_traits::allocate(alloc, newcapacity);
+  iterator newfirst_free =
+      uninitialized_move(elements, first_free, newelements);
+  free();
+  elements = newelements;
+  first_free = newfirst_free;
+  cap = newelements + newcapacity;
+}
---
+++ StrVec.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <cstddef>
+#include <memory>
+#include <string>
+#include <utility>
+
+class StrVec {
+ public:
+  using value_type = std::string;
+  using allocator_type = std::allocator<value_type>;
+  using size_type = std::size_t;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using iterator = value_type *;
+  using const_iterator = const value_type *;
+  StrVec();
+  StrVec(const StrVec &other);
+  StrVec &operator=(const StrVec &rhs);
+  ~StrVec();
+  void push_back(const_reference value);
+  size_type size() const;
+  size_type capacity() const;
+  iterator begin() const;
+  iterator end() const;
+
+ private:
+  using Alloc_traits = std::allocator_traits<allocator_type>;
+  inline static allocator_type alloc;
+  static std::pair<iterator, iterator> alloc_n_copy(const_iterator beg,
+                                                    const_iterator end);
+  static std::pair<iterator, iterator> alloc_n_move(iterator beg, iterator end);
+  void chk_n_alloc();
+  void free();
+  void reallocate();
+  iterator elements = nullptr;
+  iterator first_free = nullptr;
+  iterator cap = nullptr;
+};
