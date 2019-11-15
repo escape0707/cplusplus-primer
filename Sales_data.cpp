40a814d519c44d48a292eb23840c1836d95f4563
escape0707 <tothesong@gmail.com>
2019-11-15 11:49:36 +0800

    Finish Sales_data.cpp

--- Sales_data.cpp
+++ Sales_data.cpp
@@ -1,34 +1,39 @@
+#include <iostream>
+#include <string>
+
 #include "Sales_data.h"
 
-Sales_data() = default;
+using std::istream, std::ostream;
+using std::string;
 
-Sales_data(const std::string &s, unsigned n, double p)
+Sales_data::Sales_data() = default;
+
+Sales_data::Sales_data(const std::string &s, unsigned n, double p)
     : bookNo(s), units_sold(n), revenue(p * n) {}
 
-explicit Sales_data(const std::string &s) : bookNo(s) {}
+Sales_data::Sales_data(const std::string &s) : bookNo(s) {}
 
-explicit Sales_data(std::istream &is) {
+Sales_data::Sales_data(std::istream &is) {
   is >> *this;
 }
 
-std::string isbn() const {
+std::string Sales_data::isbn() const {
   return bookNo;
-}
-
-Sales_data &operator+=(const Sales_data &rhs);
-
-inline double Sales_data::avg_price() const {
-  if (units_sold) {
-    return revenue / units_sold;
-  } else {
-    return 0;
-  }
 }
 
 Sales_data &Sales_data::operator+=(const Sales_data &rhs) {
   units_sold += rhs.units_sold;
   revenue += rhs.revenue;
   return *this;
+}
+
+
+double Sales_data::avg_price() const {
+  if (units_sold) {
+    return revenue / units_sold;
+  } else {
+    return 0;
+  }
 }
 
 std::istream &operator>>(std::istream &is, Sales_data &item) {
