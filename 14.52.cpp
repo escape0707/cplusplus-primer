17c9ccdf8dea74d302ad0780b96717264a3f19cb
Escape0707 <tothesong@gmail.com>
2019-12-04 20:16:06 +0800

    14.9.3

---
+++ 14.52.cpp
@@ -0,0 +1,23 @@
+class SmallInt {
+  inline friend SmallInt operator+(const SmallInt &, const SmallInt &);
+
+ public:
+  SmallInt(int = 0);
+  operator int() const { return val; }
+
+ private:
+  int val;
+};
+
+struct LongDouble {
+  LongDouble operator+(const SmallInt &);
+};
+
+LongDouble operator+(LongDouble &, double);
+
+int main() {
+  SmallInt si;
+  LongDouble ld;
+  // ld = si + ld;  // can't find other members as in 14.9.2 p. 587, so no match
+  ld = ld + si;  // LongDouble::operator+(const SmallInt &) is exact match
+}
---
+++ 14.53.cpp
@@ -0,0 +1,16 @@
+class SmallInt {
+  inline friend SmallInt operator+(const SmallInt &, const SmallInt &);
+
+ public:
+  SmallInt(int = 0) {}
+  operator int() const { return val; }
+
+ private:
+  int val;
+};
+
+int main() {
+  SmallInt s1;
+  // double d = s1 + 3.14;  // ambiguous
+  __attribute__((unused)) double d = s1.operator int() + 3.14;
+}
