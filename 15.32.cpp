b6cf4411b80ec356a4cfca843642768c12bad6c1
Escape0707 <tothesong@gmail.com>
2019-12-23 20:34:09 +0800

    15.9.2

---
+++ 15.32.cpp
@@ -0,0 +1,3 @@
+// Query class is just a wrapper class to provide object like interface (java
+// like) to its underlying shared_ptr<Query_base>
+// so all the operation just operate on that shared_ptr as usual
---
+++ 15.33.cpp
@@ -0,0 +1,1 @@
+// Query_base is a pure class, so no object of that class will be created
